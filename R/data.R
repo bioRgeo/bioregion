474d142724dd1a662ee9438b1629221bd07423a2
maximelenormand <maxime.lenormand@irstea.fr>
2021-08-11 17:01:07 +0200

    Update data

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' Spatial distribution of Mediterrean Vegetation
+#'
+#' A dataset containing the
+#'
+#' @format A
+#' \describe{
+  #'   \item{vegedf}{  }
+#'   \item{vegemat}{  }
+#'   \item{vegesp}{  }
+#' }
+#' @source <http://>
+"vegedf"
+
+#' Spatial distribution of Mediterrean Vegetation
+#'
+#' A dataset containing the
+#'
+#' @format A
+#' \describe{
+#'   \item{vegedf}{  }
+#'   \item{vegemat}{  }
+#'   \item{vegesp}{  }
+#' }
+#' @source <http://>
+"vegemat"
+
+#' Spatial distribution of Mediterrean Vegetation
+#'
+#' A dataset containing the
+#'
+#' @format A
+#' \describe{
+#'   \item{vegedf}{  }
+#'   \item{vegemat}{  }
+#'   \item{vegesp}{  }
+#' }
+#' @source <http://>
+"vegesp"
+
