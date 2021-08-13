5fae19f0fbb03fa90c065be938d93a173619f5ef
maximelenormand <maxime.lenormand@irstea.fr>
2021-08-13 11:57:04 +0200

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -7,6 +7,9 @@
 #' Pierre Denelle (\email{pierre.denelle@gmail.com}),
 #' Maxime Lenormand (\email{maxime.lenormand@inrae.fr}) and
 #' Boris Leroy (\email{leroy.boris@gmail.com})
+#' @importFrom Rcpp evalCpp
+#' @importFrom RcppEigen fastLm
+#' @importFrom Rdpack c_Rd
 #' @useDynLib bioRgeo, .registration=TRUE
 #' @name bioRgeo
 NULL
