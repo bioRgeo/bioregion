6e0d387a91ae913ac589382fba97d952734cc460
maximelenormand <maxime.lenormand@irstea.fr>
2021-10-12 11:16:55 +0200

    Update data

--- R/data.R
+++ R/data.R
@@ -1,39 +1,34 @@
-#' Spatial distribution of Mediterrean Vegetation
+#' Spatial distribution of Mediterrean Vegetation (data.frame)
 #'
-#' A dataset containing the
+#' A dataset containing the abundance of 3,697 species in 715 sites.
 #'
-#' @format A
+#' @format A \code{data.frame} with 460,878 rows and 3 columns:
 #' \describe{
-  #'   \item{vegedf}{  }
-#'   \item{vegemat}{  }
-#'   \item{vegesp}{  }
+#'   \item{Site}{Unique site identifier (corresponding to the field ID of vegesp).}
+#'   \item{Species}{Unique species identifier.}
+#'   \item{W}{Species abundance}
 #' }
-#' @source <http://>
+#' @source <https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.4718>
 "vegedf"
 
-#' Spatial distribution of Mediterrean Vegetation
+#' Spatial distribution of Mediterrean Vegetation (co-occurence matrix)
 #'
-#' A dataset containing the
+#' A dataset containing the abundance of each of the 3,697 species in each of the 715 sites.
 #'
-#' @format A
-#' \describe{
-#'   \item{vegedf}{  }
-#'   \item{vegemat}{  }
-#'   \item{vegesp}{  }
-#' }
-#' @source <http://>
+#' @format A co-occurence \code{matrix} with sites as rows and species as columns. Each element of the matrix
+#' represents the abundance of the species in the site.
+#' @source <https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.4718>
 "vegemat"
 
-#' Spatial distribution of Mediterrean Vegetation
+#' Spatial distribution of Mediterrean Vegetation (spatial grid)
 #'
-#' A dataset containing the
+#' A dataset containing the geometry of the 715 sites.
 #'
 #' @format A
 #' \describe{
-#'   \item{vegedf}{  }
-#'   \item{vegemat}{  }
-#'   \item{vegesp}{  }
+#'   \item{ID}{Unique site identifier.}
+#'   \item{geometry}{Geometry of the site.}
 #' }
-#' @source <http://>
+#' @source <https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.4718>
 "vegesp"
 
