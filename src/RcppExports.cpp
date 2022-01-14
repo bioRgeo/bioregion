0f3cc3ef5c0cc18c6735851e13a42dae7a52fdd0
maximelenormand <maxime.lenormand@inrae.fr>
2022-01-14 14:45:53 +0100

    Update RcppExports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // abc
 NumericMatrix abc(NumericMatrix comat);
 RcppExport SEXP _bioRgeo_abc(SEXP comatSEXP) {
