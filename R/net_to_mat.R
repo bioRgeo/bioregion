9235e5fae9b063644e07f7b98d1debe02e6b0269
maximelenormand <maxime.lenormand@inrae.fr>
2022-02-24 16:07:23 +0100

    Update net_to_mat mat_to_net

--- R/net_to_mat.R
+++ R/net_to_mat.R
@@ -10,12 +10,12 @@
 #' @param weight a boolean indicating if the weight should be considered
 #' @param squared a boolean indicating if the output matrix should but squared (same objects in rows and columns)
 #' @param symmetrical a boolean indicating if the resulting matrix should be symmetrical (only if \code{squared = TRUE}).
-#' Note that different weights associated with two opposite pairs already present in df will be preserved.
-#' @param value the value to assign to the pairs of objects not present in df (0 by default)
+#' Note that different weights associated with two opposite pairs already present in net will be preserved.
+#' @param value the value to assign to the pairs of objects not present in net (0 by default)
 #' @export
-#' @return A \code{matrix} with the first objects (first column of \code{df}) as rows and
-#' the second objects (second column of \code{df}) as columns. Note that if \code{squared = TRUE} the rows and columns
-#' have the same number of elements corresponding to the concatenation of unique objects in  \code{df}'s first and second
+#' @return A \code{matrix} with the first objects (first column of \code{net}) as rows and
+#' the second objects (second column of \code{net}) as columns. Note that if \code{squared = TRUE} the rows and columns
+#' have the same number of elements corresponding to the concatenation of unique objects in  \code{net}'s first and second
 #' columns. If \code{squared = TRUE} the matrix can be forced to be symetrical based on the upper triangular part of
 #' the matrix.
 #' @author
@@ -24,7 +24,7 @@
 #' Boris Leroy (\email{leroy.boris@gmail.com})
 #' @seealso \link{mat_to_net}
 #' @examples
-#' df <- data.frame(
+#' net <- data.frame(
 #'   Site = c(rep("A", 2), rep("B", 3), rep("C", 2)),
 #'   Species = c("a", "b", "a", "c", "d", "b", "d"),
 #'   Weight = c(10, 100, 1, 20, 50, 10, 20)
@@ -36,7 +36,7 @@ net_to_mat <- function(net, weight = FALSE, squared = FALSE, symmetrical = FALSE
 
   # Controls
   if (!is.data.frame(net)) {
-    stop("df must be a two- or three-columns data.frame")
+    stop("net must be a two- or three-columns data.frame")
   }
 
   sco <- sum(is.na(net))
@@ -93,7 +93,7 @@ net_to_mat <- function(net, weight = FALSE, squared = FALSE, symmetrical = FALSE
     net$Weight <- 1
   }
 
-  # Modify df to obtain a squared contingency table
+  # Modify net to obtain a squared contingency table
   if (squared) {
     diff12 <- setdiff(idobj1, idobj2) # Objects contains in the first column but not in the second
     diff21 <- setdiff(idobj2, idobj1) # Objects contains in the second column but not in the first
@@ -101,14 +101,14 @@ net_to_mat <- function(net, weight = FALSE, squared = FALSE, symmetrical = FALSE
     # Create a new dataframe containing all objects in both columns
     obj12 <- data.frame(Object1 = net$Object1[1], Object2 = diff12, Weight = NA)
     obj21 <- data.frame(Object1 = diff21, Object2 = net$Object2[1], Weight = NA)
-    df <- rbind(df, obj12, obj21)
+    net <- rbind(net, obj12, obj21)
 
     # Extract unique objects in both columns (should be the same at this stage)
     idobj1 <- net$Object1[!duplicated(net$Object1)]
     idobj2 <- idobj1
   }
 
-  # Create contingency table from df
+  # Create contingency table from net
   net$Object1 <- factor(net$Object1, levels = idobj1)
   net$Object2 <- factor(net$Object2, levels = idobj2)
 
@@ -136,8 +136,8 @@ net_to_mat <- function(net, weight = FALSE, squared = FALSE, symmetrical = FALSE
 
   # Check for empty rows and columns if squared = FALSE
   # if(!squared){
-  #  comat <- comat[rowSums(comat) > 0,]
-  #  comat <- comat[, colSums(comat) > 0]
+  #  mat <- mat[rowSums(mat) > 0,]
+  #  mat <- mat[, colSums(mat) > 0]
   # }
 
   # Return the contingency matrix
