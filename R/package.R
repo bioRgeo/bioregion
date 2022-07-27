5436f9cbd30bded96c2d0f43527ff12824cc1d4f
maximelenormand <maxime.lenormand@inrae.fr>
2022-07-27 12:18:06 +0200

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -8,10 +8,11 @@
 #' Maxime Lenormand (\email{maxime.lenormand@inrae.fr}) and
 #' Boris Leroy (\email{leroy.boris@gmail.com})
 #' @importFrom graphics plot
-#' @importFrom Rcpp evalCpp
-#' @importFrom Rdpack c_Rd
 #' @importFrom MASS fbeta
 #' @importFrom mathjaxr preview_rd
+#' @importFrom mclust bic
+#' @importFrom Rcpp evalCpp
+#' @importFrom Rdpack c_Rd
 #' @importFrom rmarkdown draft
 #' @useDynLib bioRgeo, .registration=TRUE
 #' @name bioRgeo
