d7276e2d63cb06e87645c6b4e67a4641798132ad
denelle <pierre.denelle@gmail.com>
2023-01-30 21:20:04 +0100

    benthic invertebrates dataset deleted

--- R/data.R
+++ R/data.R
@@ -34,40 +34,6 @@
 #' @source <https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.4718>
 "vegesp"
 
-#' Spatial distribution of benthic invertebrates in Brittany (data.frame)
-#'
-#' A dataset containing the abundance of 3,697 species in 715 sites.
-#'
-#' @format A `data.frame` with 28,563 rows and 3 columns:
-#' \describe{
-#'   \item{Site}{Unique site identifier (corresponding to the field ID of benthicsp).}
-#'   \item{Species}{Unique species identifier.}
-#'   \item{Abundance}{Species abundance}
-#' }
-"benthicdf"
-
-#' Spatial distribution of benthic invertebrates in Brittany
-#' (co-occurrence matrix)
-#'
-#' A dataset containing the abundance of each of the 838 species in each of
-#' the 162 sites.
-#'
-#' @format A co-occurrence `matrix` with sites as rows and species as
-#' columns. Each element of the matrix
-#' represents the abundance of the species in the site.
-"benthicmat"
-
-#' Spatial distribution of benthic invertebrates in Brittany (spatial points)
-#'
-#' A dataset containing the geometry of the 162 sites.
-#'
-#' @format A
-#' \describe{
-#'   \item{ID}{Unique site identifier.}
-#'   \item{geometry}{Geometry of the site.}
-#' }
-"benthicsp"
-
 #' Spatial distribution of fish in Europe (data.frame)
 #'
 #' A dataset containing the abundance of 195 species in 338 sites.
