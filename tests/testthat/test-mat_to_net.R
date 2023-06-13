6793fcab596a211b0561b5e64fc4b17ea8ca0cb6
maximelenormand <maxime.lenormand@inrae.fr>
2023-06-13 09:53:12 +0200

    Reinitializing automated tests

--- tests/testthat/test-mat_to_net.R
+++ tests/testthat/test-mat_to_net.R
@@ -3,6 +3,7 @@ mat <- matrix(sample(1000, 50), 5, 10)
 rownames(mat) <- paste0("Site", 1:5)
 colnames(mat) <- paste0("Species", 1:10)
 
+
 # Tests for valid outputs -----------------------------------------------------
 test_that("dimension of output", {
   net <- mat_to_net(mat, weight = TRUE)
