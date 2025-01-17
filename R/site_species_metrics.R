95d50a274d14a78c01cbcebf059dcf0c392a27ea
denelle <pierre.denelle@gmail.com>
2025-01-17 11:04:33 +0100

    site_species_metrics: example simplified

--- R/site_species_metrics.R
+++ R/site_species_metrics.R
@@ -99,10 +99,6 @@
 #' Maxime Lenormand (\email{maxime.lenormand@inrae.fr}) 
 #' 
 #' @examples
-#' comat <- matrix(sample(1000, 50), 5, 10)
-#' rownames(comat) <- paste0("Site", 1:5)
-#' colnames(comat) <- paste0("Species", 1:10)
-#' 
 #' comat <- matrix(sample(0:1000, size = 500, replace = TRUE, prob = 1/1:1001),
 #'                 20, 25)
 #' rownames(comat) <- paste0("Site",1:20)
